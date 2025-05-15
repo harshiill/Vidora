5deb4e171e8244c996f20b2162e2406535187235
harshiill <harshilkhandelwal28@gmail.com>
2025-05-15 22:47:58 +0530

    connection to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,46 @@
+ //require('dotenv').config({path: './.env' });
+
+ import dotenv from "dotenv";
+ dotenv.config({ path: "./.env" });
+ 
+ import mongoose from "mongoose";
+ import { DB_NAME } from "./constants.js";
+ import connectDB from "./db/index.js";
+
+ connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*
+ import express from "express";
+ const app=express();
+
+( async () => {
+    try {
+        await mongoose.connect( `${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error",()=>
+        {
+            console.log("Error in connection");
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+}) ()
+
+*/

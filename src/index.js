5002dd9aabe7e0b2b0acd66749654e5c2867298e
harshiill <harshilkhandelwal28@gmail.com>
2025-05-16 23:22:34 +0530

    asyncHandler+Configuration

--- src/index.js
+++ src/index.js
@@ -7,7 +7,17 @@
  import { DB_NAME } from "./constants.js";
  import connectDB from "./db/index.js";
 
- connectDB();
+ connectDB()
+ .then(() => 
+{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+ .catch((error) => {
+     console.error("MongoDB connection error:", error);
+     process.exit(1); // Exit the process with failure
+ });
 
 
 

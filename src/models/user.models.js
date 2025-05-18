df400f5fbade155087519c5bb261d8b71c1f627d
harshiill <harshilkhandelwal28@gmail.com>
2025-05-18 23:46:30 +0530

    bcrypt And JWT config and user and video Model Schema Setup

---
+++ src/models/user.models.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+     email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true    
+    },
+    avatar:{
+        type:String, //Cludinary URL
+        required:true,
+    },
+    coverImage:{
+        type:String, //Cludinary URL
+    },
+    watchHistory:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+        trim:true,
+    },
+    refreshToken:{
+        type:String,
+        
+    },
+    
+},{timestamps:true});
+
+userSchema.pre("save",async function(next)
+{
+    if(this.isModified("password"))
+    {
+       this.password=await bcrypt.hash(this.password,10);
+    }
+    next(); 
+} )
+
+userSchema.methods.isPasswordCorrect=async function(password)
+{
+    return await bcrypt.compare(password,this.password)
+}
+export const User=mongoose.model("User",userSchema);
---
+++ src/models/video.models.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+import Schema from 'mongoose';''
+
+const videoSchema = new Schema({
+    videoFile:{
+        type:String,//Cludinary URL
+        required:true,
+    }
+    ,
+    thumbnail:{
+        type:String,//Cludinary URL
+        required:true,
+    },
+    title:{
+        type:String,
+        required:true,
+
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    duration:{
+        type:Number, //Cludinary 
+        required:true,
+    },
+    views:{
+        type:Number,
+        default:0,
+    },
+    isPublished:{
+        type:Boolean,
+        default:true,
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    }
+},{timestamps:true});
+
+video.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model('Video', videoSchema);
